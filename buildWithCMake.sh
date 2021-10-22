4a1e545844b9561ebbb6ad66d5054153d9fd0d98
peter-simonson <peter.t.simonson@gmail.com>
2021-10-21 21:50:48 -0600

    added submodule init to buildWithCMake.sh

--- buildWithCMake.sh
+++ buildWithCMake.sh
@@ -1,5 +1,7 @@
-#Running this script will build the project with CMake
+#Running this script will build the project with CMake. It will also initialize any git submodules
 #Note if you do not have cmake or do not wish to use CMake there should be a MakeFile already in cmake-build-debug
+
+git submodule update --init --recursive
 
 cd cmake-build-debug || exit
 
