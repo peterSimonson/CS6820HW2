d30a3ba92ebbd586335a0d49f3b6b0ed2da3431c
peter-simonson <peter.t.simonson@gmail.com>
2021-12-05 19:19:03 -0700

    created Tokens.h to store an enum containing all tokens

---
+++ LL1Lib/Tokens.h
@@ -0,0 +1,69 @@
+//
+// Created by Peter Simonson on 12/5/21.
+//
+
+#ifndef CS6820HW2_TOKENS_H
+#define CS6820HW2_TOKENS_H
+
+///This contains all of the terminals and non-terminals in our grammar
+///Note: non-terminals are list first. Terminals occur after NUM_OF_NON_TERMINALS enum
+enum tokens{
+    START_TOKEN,
+    LINE_FULL_TOKEN,
+    LINE_VAR_NAME,
+    LINE_VAR_NAME_REMAINING,
+    PROCEDURE_PARAMS_TOKEN,
+    PROCEDURE_ARGS_TOKEN,
+    PARAMS_TOKEN,
+    ARGS_TOKEN,
+    MORE_PARAMS_TOKEN,
+    MORE_ARGS_TOKEN,
+    EXPR_TOKEN,
+    L_TERM_ADD_SUB_TOKEN,
+    R_TERM_ADD_SUB_TOKEN,
+    ADD_SUB_PRIME_TOKEN,
+    L_TERM_MULT_DIV_TOKEN,
+    R_TERM_MULT_DIV_TOKEN,
+    MULT_DIV_PRIME_TOKEN,
+    MULT_AND_RIGHT_OP_TOKEN,
+    DIV_AND_RIGHT_OP_TOKEN,
+    L_TERM_POWER_TOKEN,
+    R_TERM_POWER_TOKEN,
+    POWER_PRIME_TOKEN,
+    POWER_AND_RIGHT_OP_TOKEN,
+    G_FACTOR_TOKEN,
+    PARENS_EXPR_TOKEN,
+    POSVAL_TOKEN,
+    SPACE_NEG_VAL_TOKEN,
+    VAR_TYPE_TOKEN,
+    //Define Columns of our table
+    NUM_OF_NON_TERMINALS,
+    END_TOKEN  = NUM_OF_NON_TERMINALS,
+    PROCEDURE_TOKEN,
+    COMMA_TOKEN,
+    EQUALS_TOKEN,
+    PLUS_TOKEN,
+    MINUS_TOKEN,
+    MULTIPLY_TOKEN,
+    DIVIDE_TOKEN,
+    POWER_TOKEN,
+    OPEN_PAREN_TOKEN,
+    CLOSE_PAREN_TOKEN,
+    OPEN_CURLY_TOKEN,
+    CLOSE_CURLY_TOKEN,
+    NAME_TOKEN,
+    NUM_TOKEN,
+    SPACE_NEG_NAME_TOKEN,
+    SPACE_NEG_NUM_TOKEN,
+    NEG_NAME_TOKEN,
+    NEG_NUM_TOKEN,
+    DATA_TYPE_TOKEN,
+    RETURN_TOKEN,
+    EPSILON_TOKEN,
+    //TODO: Change where this is used so it is the number of tokens not number of tokens - 1
+    NUM_OF_TOKENS = EPSILON_TOKEN,
+    ERROR_TOKEN = -1
+};
+
+
+#endif //CS6820HW2_TOKENS_H
